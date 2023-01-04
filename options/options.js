8b1652a95ce40687b0f26838ab864644e50dd93c
Crowdbotics <team@crowdbotics.com>
2023-01-04 18:26:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digime-demo-38183",
+  url: "https://digime-demo-38183.botics.co",
+  api: "https://digime-demo-38183.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
