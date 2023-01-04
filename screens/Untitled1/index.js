53ee4f730e12a34b045cb577eebc76f8098d04e3
Han Mercer <hanmercer22@gmail.com>
2023-01-04 22:55:48 +0800

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }} style={styles.wyUkXMOR}></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  wyUkXMOR: {
+    borderRadius: 0
+  },
+  HBVdeYSO: {
+    height: 182,
+    width: 377,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  ZvLQMFTy: {
+    height: 574,
+    width: 361,
+    backgroundColor: "#ffcc00",
+    borderRadius: 0,
+    color: "#777777",
+    top: 0,
+    left: -2,
+    position: "absolute"
+  },
+  sdvugZjP: {
+    width: 143,
+    height: 73
+  },
+  NOKVLIbr: {
+    width: 359,
+    height: 573
+  }
+});
+export default Untitled1;
